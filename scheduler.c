9ecd69cf9a5b36aaeab088fd8fe0f24f761d055d
AlphaGameKnight <alphagameknight1517@hotmail.com>
2023-03-23 16:02:42 -0500

    Added comments for different sections

--- scheduler.c
+++ scheduler.c
@@ -1,6 +1,58 @@
+/**********************************************************************/
+/*                                                                    */
+/* Program Name: scheduler - Simulate an operating system scheduler   */
+/* Author:       William S. Wu                                        */
+/* Installation: Pensacola Christian College, Pensacola, Florida      */
+/* Course:       CS326, Operating Systems                             */
+/* Date Written: March 23, 2023                                       */
+/*                                                                    */
+/**********************************************************************/
+
+/**********************************************************************/
+/*                                                                    */
+/* I pledge this assignment is my own first time work.                */
+/* I pledge I did not copy or try to copy any code from the Internet. */
+/* I pledge I did not copy or try to copy any code from anyone else.  */
+/* I pledge I did not attempt to give code to anyone else on this.    */
+/* I understand if I violate this pledge I will receive a 0 grade.    */
+/*                                                                    */
+/*                                                                    */
+/*                      Signed: _____________________________________ */
+/*                                           (signature)              */
+/*                                                                    */
+/*                                                                    */
+/**********************************************************************/
+
+/**********************************************************************/
+/*                                                                    */
+/* This program asks for a range of whole number values and           */
+/* recursively sums all even numbers within the range inclusively.    */
+/* Then, it will print that sum along with information about the      */
+/* recursive action of the function.                                  */
+/*                                                                    */
+/**********************************************************************/
+
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
+
+/**********************************************************************/
+/*                         Symbolic Constants                         */
+/**********************************************************************/
+
+/**********************************************************************/
+/*                         Function Prototypes                        */
+/**********************************************************************/
+
+/**********************************************************************/
+/*                            Main Function                           */
+/**********************************************************************/
 int main()
 {
-   printf("Ooga Booga");
 
    return 0;
 }
+
+/**********************************************************************/
+/*                      Print the program heading                     */
+/**********************************************************************/
